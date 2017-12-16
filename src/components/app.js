a6a23924368549e8e46b0f5632897758628cb484
jonjian <jonjian@gmail.com>
2017-12-16 11:09:59 -0800

    work on dynamic search bar

--- src/components/app.js
+++ src/components/app.js
@@ -3,11 +3,14 @@ angular.module('video-player')
 .component('app', {
 
   controller: function() {
-    this.videos = window.exampleVideoData;
+    this.videos = exampleVideoData;
+    this.key = YOUTUBE_API_KEY;
     this.selectVideo = (index) => {
       this.currentVideo = this.videos[index];
     };//will be our on-click. Will change current video
-    this.searchResults = () => {};
+    this.searchResults = (data) => {
+      this.videos = data;
+    };
     this.currentVideo = this.videos[0];
   },
 
@@ -15,15 +18,15 @@ angular.module('video-player')
   `<div id="app container">
     <nav class="navbar">
       <div class="col-md-6 col-md-offset-3">
-        <search><h5><em>search</em> component goes here</h5></search>
+        <search search-results="$ctrl.searchResults"></search>
       </div>
     </nav>
     <div class="row">
       <div class="col-md-7">
-        <video-player video="$ctrl.currentVideo"><h5><em>videoPlayer</em> component goes here</h5></video-player>
+        <video-player video="$ctrl.currentVideo"></video-player>
       </div>
       <div class="col-md-5">
-        <video-list videos="$ctrl.videos" select-video="$ctrl.selectVideo"><h5><em>videoList</em> component goes here</h5></video-list>
+        <video-list videos="$ctrl.videos" select-video="$ctrl.selectVideo"></video-list>
       
       </div>
     <div>
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,5 +1,62 @@
 angular.module('video-player')
-.service('youTube', function(){
+.service('youTube', function($http) {
   // TODO
-  this.search = () => {};
+  this.search = function (query, callback) {
+    $http.get('https://www.googleapis.com/youtube/v3/search', {
+      params: {
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        method: 'GET',
+        key: YOUTUBE_API_KEY,
+        type: 'video',
+        maxResults: 5,
+        part: 'snippet',
+        q: query,
+        data: 'json',
+        success: function(data) {
+          callback(data);
+          console.log('IT WORKS! mom get the camera');
+        },
+        error: function(error) {
+          console.log(error);
+        }
+      }
+
+    });
+  };
 });
+
+
+
+// $scope.search = function (isNewQuery) {
+//   $scope.loading = true;
+//   $http.get('https://www.googleapis.com/youtube/v3/search', {
+//     params: {
+//       key: YOUR_API_KEY_HERE,
+//       type: 'video',
+//       maxResults: '10',
+//       pageToken: isNewQuery ? '' : $scope.nextPageToken,
+//       part: 'id,snippet',
+//       fields: 'items/id,items/snippet/title,items/snippet/description,
+//               items/snippet/thumbnails/default,items/snippet/channelTitle,
+//               nextPageToken',
+//       q: this.query
+//     }
+//   })
+//   .success( function (data) {
+//     if (data.items.length === 0) {
+//       $scope.label = 'No results were found!';
+//     }
+//     VideosService.listResults(data, $scope.nextPageToken && !isNewQuery);
+//     $scope.nextPageToken = data.nextPageToken;
+//     $log.info(data);
+//   })
+//   .error( function () {
+//     $log.info('Search error');
+//   })
+//   .finally( function () {
+//     $scope.loadMoreButton.stopSpin();
+//     $scope.loadMoreButton.setDisabled(false);
+//     $scope.loading = false;
+//   })
+//   ;
+// };
