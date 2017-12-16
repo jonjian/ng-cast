57d5a0f85605d3950f287e9a5607814fe34ede4e
jonjian <jonjian@gmail.com>
2017-12-16 15:55:22 -0800

    finish basic req

--- src/components/search.js
+++ src/components/search.js
@@ -9,11 +9,18 @@ angular.module('video-player')
   controller: function(youTube) {
     this.service = youTube;
     this.newQuery = '';
+    this.liveSearch = (query, callback) => {
+      this.service.search(query, callback);
+    };
   },
   template: 
 `<div class="search-bar form-inline">
-  <input class="form-control" type="text" ng-model="$ctrl.newQuery"/>
-  <button  ng-click="$ctrl.service.search($ctrl.newQuery, $ctrl.searchResults)" class="btn">
+  <input class="form-control" type="text" 
+  ng-keyDown="$ctrl.liveSearch($ctrl.newQuery, $ctrl.searchResults)"
+  ng-model-options='{debounce: 500}'
+  ng-change="$ctrl.liveSearch($ctrl.newQuery, $ctrl.searchResults)"
+  ng-model="$ctrl.newQuery"/>
+  <button ng-click="$ctrl.service.search($ctrl.newQuery, $ctrl.searchResults)" class="btn">
     <span class="glyphicon glyphicon-search"></span>
   </button>
 </div>`
