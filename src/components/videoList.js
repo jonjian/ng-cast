bcd2cdaeebe5c72f52cd3fc7cceeab2fe5768690
jonjian <jonjian@gmail.com>
2017-12-15 16:16:48 -0800

    Connect components together for the first time
    they now render on index.html
    partial work on videoList, setting up for videoListEntries

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,42 @@
 angular.module('video-player')
 .component('videoList', {
-  // TODO
+  bindings: {
+    videos: '<'
+  },
+
+  template: 
+  `<h3>RECOMMENDED FOR YOU...</h3>
+    <video-list-entry 
+      index="$index" 
+      video="video"
+      ng-repeat="video in $ctrl.videos track by $index"
+    />
+  
+
+
+  `
 });
+
+// `<ul class="video-list">
+//   <video-list-entry><h5><em>videoListEntry</em> component goes here</h5></video-list-entry>
+//   <video-list-entry><h5><em>videoListEntry</em> component goes here</h5></video-list-entry>
+//   <video-list-entry><h5><em>videoListEntry</em> component goes here</h5></video-list-entry>
+//   <video-list-entry><h5><em>videoListEntry</em> component goes here</h5></video-list-entry>
+//   <video-list-entry><h5><em>videoListEntry</em> component goes here</h5></video-list-entry>
+// </ul>`
+
+// template: `
+//     <h1>Todo List</h1>
+//     <input ng-model="$ctrl.newTodo">
+//     <button ng-click="$ctrl.addTodo()">add</button>
+//     <ul>
+//       <entry
+//         item="todo"
+//         index="$index"
+//         on-click="$ctrl.removeTodo"
+//         ng-repeat="todo in $ctrl.todos track by $index"
+//       />
+//     </ul>
+//     <hr/>
+//   `
+// })
